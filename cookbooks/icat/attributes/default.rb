88c3cf6d43d9765017aa85e7393bc8a4ff343c84
Steve Androulakis <steve.androulakis@gmail.com>
2013-03-21 16:02:14 +0000

    Fixups for NeCTAR chef solo (non vagrant)

--- cookbooks/icat/attributes/default.rb
+++ cookbooks/icat/attributes/default.rb
@@ -1,1 +1,9 @@
+include_attribute 'apt'
+include_attribute 'build-essential'
+include_attribute 'subversion'
+include_attribute 'java'
+include_attribute 'glassfish'
+
 default['icat']['glassfish_password'] = 'adminadmin'
+default['java']['oracle']['accept_oracle_download_terms'] = true
+
--- cookbooks/icat/metadata.rb
+++ cookbooks/icat/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "ICAT"
+maintainer_email "steve.androulakis@monash.edu"
 license          "Apache 2.0"
 description      "Installs/Configures icat"
 
 version          "0.1"
+
+depends 'glassfish'
